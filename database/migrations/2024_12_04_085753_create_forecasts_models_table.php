1187fb16f5e1972a4ed84ea8059c296ea0fdde7b
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-12-04 10:05:08 +0100

    * Create ForecastsModel and migration

---
+++ database/migrations/2024_12_04_085753_create_forecasts_models_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forecasts_models', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('city_id');
+            $table->float('tempereture');
+            $table->date('date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forecasts_models');
+    }
+};
