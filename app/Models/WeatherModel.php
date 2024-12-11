4f091debdbad3e0f122d4a164e6fe33a85a9eefc
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-12-11 07:47:23 +0100

    * Edit name (weatherModel -> WeatherModel)

--- app/Models/WeatherModel.php
+++ app/Models/WeatherModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class weatherModel extends Model
+class WeatherModel extends Model
 {
     protected $table = "weather";
 
