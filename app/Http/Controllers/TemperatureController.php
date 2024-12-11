fbfadc63d2a22fb28e5fc1dfcc243d2e1ecba9d5
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-12-11 08:19:13 +0100

    * Code cleanup

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -7,15 +7,14 @@ class TemperatureController extends Controller
     public function index($city)
     {
 
-        $forecast= [
+        $forecast = [
             "beograd" => [22, 24, 25, 20, 18],
             "sarajevo" => [20, 24, 22, 22, 25]
         ];
 
         // beograd -> BEOGRAD -> bEOGRAD
         $city = strtolower($city);
-        if(!array_key_exists($city, $forecast))
-        {
+        if (!array_key_exists($city, $forecast)) {
             die("City not exists");
         }
 
