d4b93a9323b7ef3ff87ce4ac4700d68738dafa67
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2025-01-16 07:23:04 +0100

    * Bootstrap

--- resources/views/admin/forecast.blade.php
+++ resources/views/admin/forecast.blade.php
@@ -1,39 +1,74 @@
-<form method="POST" action="{{route("forecast.save")}}">
 
-    {{csrf_field()}}
+@extends("admin.layout")
 
-    <select name="city_id">
+@section("content")
+
+    <form method="POST" action="{{route("forecast.save")}}">
+
+        <h2> Create new forecast </h2>
+        {{csrf_field()}}
+
+        <div class="mb-3 col-md-5">
+            <select name="city_id" class="form-select">
+                @foreach(\App\Models\Cities::all() as $city)
+                    <option value="{{$city->id}}">{{$city->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mb-3 col-md-5">
+        <input class="form-control" type="text" name="temperature" placeholder="Enter temperature">
+        </div>
+
+        <div class="mb-3 col-md-5">
+            <select name="weather_type" class="form-select">
+                @foreach(\App\Models\ForecastsModel::WEATHERS as $weather)
+                    <option>{{ $weather }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mb-3 col-md-5">
+        <input class="form-control" type="number" name="probability" placeholder="Enter the precipitation chance">
+        </div>
+
+        <div class="mb-3 col-md-5">
+        <input class="form-control" type="date" name="forecast_date">
+        </div>
+
+        <div class="col-md-5">
+            <button class="btn btn-outline-primary col-12">Save</button>
+        </div>
+
+    </form>
+
+    <div class="d-flex flex-wrap" style="gap: 10px">
         @foreach(\App\Models\Cities::all() as $city)
-            <option value="{{$city->id}}">{{$city->name }}</option>
+            <div>
+                <p>{{ $city->name }}</p>
+
+                <ul class="list-group  mb-4">
+                    @foreach($city->forecasts as $forecast)
+
+                        @php $color= \App\Http\ForecastHelper::getColorByTemperature($forecast->temperature);
+                            $icon = \App\Http\ForecastHelper::getIconByWeatherType($forecast->weather_type)
+                        @endphp
+
+                        <li class="list-group-item ">{{$forecast->forecast_date}} -
+                            <i class="fa-solid {{$icon}}"  ></i>
+                            <span style="color: {{$color}}">{{$forecast->temperature}} </span></li>
+                    @endforeach
+                </ul>
+            </div>
         @endforeach
-    </select>
-
-    <input type="text" name="temperature" placeholder="Enter temperature">
-
-    <select name="weather_type">
-        @foreach(\App\Models\ForecastsModel::WEATHERS as $weather)
-            <option>{{ $weather }}</option>
-        @endforeach
-    </select>
-
-    <input type="number" name="probability" placeholder="Enter the precipitation chance">
-
-    <input type="date" name="forecast_date">
-
-    <button>Save</button>
-</form>
-
-@foreach(\App\Models\Cities::all() as $city)
-
-    <p>{{ $city->name }}</p>
-    <ul>
-        @foreach($city->forecasts as $forecast)
-
-            @php $color= \App\Http\ForecastHelper::getColorByTemperature($forecast->temperature) @endphp
+    </div>
 
 
-            <li>{{$forecast->forecast_date}} - <span style="color: {{$color}}">{{$forecast->temperature}}</span></li>
-        @endforeach
-    </ul>
+@endsection
 
-@endforeach
+
+<i class="fa-solid fa-sun"></i>
+<i class="fa-solid fa-cloud-rain"></i>
+<i class="fa-solid fa-snowflake"></i>
+
+
