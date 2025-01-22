11186ff2b7db3b798449efab936e60030b3e8824
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2025-01-22 08:25:57 +0100

    * Create search blade

---
+++ resources/views/search_results.blade.php
@@ -0,0 +1,11 @@
+@extends("layout")
+
+@section("content")
+
+    <div class="d-flex flex-wrap container">
+    @foreach($cities as $city)
+        <p><a class="btn btn-primary text-white m-2" href="{{ route("forecast.permalink", ['city' => $city->name]) }}">{{$city->name}}</a></p>
+    @endforeach
+    </div>
+
+@endsection
