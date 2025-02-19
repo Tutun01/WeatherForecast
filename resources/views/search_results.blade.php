b49cc66e037deaeb3c0f3ccae6139bf2e88ed670
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2025-02-19 10:49:47 +0100

    * Edit page

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -3,13 +3,26 @@
 @section("content")
 
     <div class="d-flex flex-wrap container">
+
+        @if(\Illuminate\Support\Facades\Session::has('error'))
+            <p class="text-danger fw-bold col-12">{{ \Illuminate\Support\Facades\Session::get('error') }}</p>
+            <a class="btn btn-primary" href="/login">Login</a>
+        @endif
+
     @foreach($cities as $city)
-
-
 
         @php $icon =\App\Http\ForecastHelper::getIconByWeatherType($city->todaysForecast->weather_type); @endphp
 
-        <p><a class="btn btn-primary text-white m-2" href="{{ route("forecast.permalink", ['city' => $city->name]) }}">
+        <p>
+            @if(in_array($city->id, $userFavourites))
+                <a class="btn btn-primary" href="{{route("city.favourite", ['city' => $city->id]) }}">
+                    <i class="fa-solid fa-trash"></i></a>
+            @else
+                <a class="btn btn-primary" href="{{route("city.favourite", ['city' => $city->id]) }}">
+                    <i class="fa-regular text-white fa-heart"></i></a>
+            @endif
+
+            <a class="btn btn-primary text-white me-4" href="{{ route("forecast.permalink", ['city' => $city->name]) }}">
                 <i class= "fa-solid {{ $icon }}"></i> {{$city->name}}
             </a></p>
 
