c1c003ea5ec29da1eab8b1f49a219b27c45065ef
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2025-02-21 13:47:54 +0100

    * create function favourite()

--- app/Models/UserCities.php
+++ app/Models/UserCities.php
@@ -9,4 +9,11 @@ class UserCities extends Model
 {
     protected $table = "user_cities";
     protected $fillable = ["user_id", "city_id"];
+
+    public function favourite()
+    {
+        return $this->hasOne(Cities::class, 'id', 'city_id');
+    }
+
 }
+
