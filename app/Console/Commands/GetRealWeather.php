cf88f378545be99cc9aa731c9c7c46e9cfce3fef
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2025-02-25 09:58:27 +0100

    * Adding a new forecast depending on whether the city exists

--- app/Console/Commands/GetRealWeather.php
+++ app/Console/Commands/GetRealWeather.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Cities;
+use App\Models\ForecastsModel;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
@@ -26,12 +28,22 @@ class GetRealWeather extends Command
      */
     public function handle()
     {
+
+        $city = $this->argument("city");
+
+            $dbCity = Cities::where(['name' => $city])->first();
+
+        if($dbCity === null)
+        {
+            $dbCity = Cities::create(['name' => $city]);
+        }
+
         //method: Swagger Tool
         $url = env("WEATHER_API_URL")."v1/forecast.json";
 
         $params = [
             "key" => env("WEATHER_API_KEY"),
-            "q" => $this->argument("city"),
+            "q" => $city,
             "aqi" => "no",
             "lang" =>"sr",
             "days" => 5
@@ -45,5 +57,29 @@ class GetRealWeather extends Command
         {
             $this->output->error($jsonResponse['error']['message']);
         }
+
+        if ($dbCity->todaysForecast !== null)
+        {
+            $this->output->comment("Command finished");
+            return;
+        }
+
+        $forecast = $jsonResponse["forecast"]["forecastday"][0];
+
+        $forecastDate = $forecast["date"];
+        $forecastTemperature = $forecast["day"]["avgtemp_c"];
+        $forecastWeatherType = $forecast["day"]["condition"]["text"];
+        $probability = $forecast["day"]["daily_chance_of_rain"];
+
+        $forecast = [
+            "city_id" => $dbCity->id,
+            "temperature" => $forecastTemperature,
+            "forecast_date" => $forecastDate,
+            "weather_type" => strtolower($forecastWeatherType),
+            "probability" => $probability,
+            ];
+
+        ForecastsModel::create($forecast);
+        $this->output->comment("Add new forecast");
     }
 }
